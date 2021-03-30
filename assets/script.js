0f89facd578d8e663aa3f18dba1950f92164ab24
Jay Yousef <jay.yousef@gmail.com>
2021-03-29 23:40:45 -0500

    Adding finished portfolio page

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+
+// Initialize all div with carousel class
+let carousels = bulmaCarousel.attach('.carousel', options);
+
+// Loop on each carousel initialized
+for(let i = 0; i < carousels.length; i++) {
+	// Add listener to  event
+	carousels[i].on('before:show', state => {
+		console.log(state);
+	});
+}
+
+// Access to bulmaCarousel instance of an element
+let element = document.querySelector('#carousel');
+if (element && element.bulmaCarousel) {
+	// bulmaCarousel instance is available as element.bulmaCarousel
+	element.bulmaCarousel.on('before-show', function(state) {
+		console.log(state);
+	});
+}
+bulmaCarousel.attach('#carousel-demo', {
+  slidesToScroll: 1,
+  slidesToShow: 4
+});
